710774ecf566210c9494792fc0043c0c3f577e7e
gg110400 <giuliagiudici120@gmail.com>
2024-05-10 18:29:28 +0200

    modifica numero 1

--- gestioneProdotti.js
+++ gestioneProdotti.js
@@ -17,7 +17,6 @@ window.onload = async () => {
     document.querySelector("#price").value = product.price;
     document.querySelector(".btn-light").remove();
   } else {
-  
   }
 };
 
@@ -43,52 +42,47 @@ const createNew = async () => {
   }
 };
 
-
-  const editProduct = async () => {
-    const product = {
-      name: document.getElementById("modified-name").value,
-      description: document.getElementById("modified-description").value,
-      brand: document.getElementById("modified-brand").value,
-      imageUrl: document.getElementById("modified-imageUrl").value,
-      price: document.getElementById("modified-price").value,
-      id: document.querySelector("#id").value
-    };
-    let res = await fetch(BASE_URL + product.id, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-        authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNhMDBlYzBiM2IyNTAwMTUxYjU0MTciLCJpYXQiOjE3MTUwNzczNTYsImV4cCI6MTcxNjI4Njk1Nn0.iwXD7GdljBWiTHnH4IBnnIag816mwq4QDDic5ijmNHo",
-      },
-      body: JSON.stringify(product),
-    });
-    if (res.ok) {
-      alert("Prodotto modificato");
-  
-    }
+const editProduct = async () => {
+  const product = {
+    name: document.getElementById("modified-name").value,
+    description: document.getElementById("modified-description").value,
+    brand: document.getElementById("modified-brand").value,
+    imageUrl: document.getElementById("modified-imageUrl").value,
+    price: document.getElementById("modified-price").value,
+    id: document.querySelector("#modified-id").value,
   };
+  let res = await fetch(BASE_URL + product.id, {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+      authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNhMDBlYzBiM2IyNTAwMTUxYjU0MTciLCJpYXQiOjE3MTUwNzczNTYsImV4cCI6MTcxNjI4Njk1Nn0.iwXD7GdljBWiTHnH4IBnnIag816mwq4QDDic5ijmNHo",
+    },
+    body: JSON.stringify(product),
+  });
+  if (res.ok) {
+    alert("Prodotto modificato");
+  }
+};
 
-  // Chiamata alla funzione editProduct al click di un pulsante o altro evento
+// Chiamata alla funzione editProduct al click di un pulsante o altro evento
 
-
-  const deleteProduct = async () => {
-    let id = document.querySelector("#delete-product-id").value;
-    console.log(id);
-    let res = await fetch(BASE_URL + id, {
-      method: "DELETE",
-      headers: {
-        authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNhMDBlYzBiM2IyNTAwMTUxYjU0MTciLCJpYXQiOjE3MTUwNzczNTYsImV4cCI6MTcxNjI4Njk1Nn0.iwXD7GdljBWiTHnH4IBnnIag816mwq4QDDic5ijmNHo",
-      },
-    });
-    if (res.ok) {
-      alert("Prodotto eliminato con successo");
-    } else {
-      alert("Errore durante l'eliminazione del prodotto");
-    
-    }
-  };
-  
+const deleteProduct = async () => {
+  let id = document.querySelector("#delete-product-id").value;
+  console.log(id);
+  let res = await fetch(BASE_URL + id, {
+    method: "DELETE",
+    headers: {
+      authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNhMDBlYzBiM2IyNTAwMTUxYjU0MTciLCJpYXQiOjE3MTUwNzczNTYsImV4cCI6MTcxNjI4Njk1Nn0.iwXD7GdljBWiTHnH4IBnnIag816mwq4QDDic5ijmNHo",
+    },
+  });
+  if (res.ok) {
+    alert("Prodotto eliminato con successo");
+  } else {
+    alert("Errore durante l'eliminazione del prodotto");
+  }
+};
 
 const fetchData = async () => {
   try {
@@ -106,35 +100,33 @@ const fetchData = async () => {
     let container = document.getElementById("cards-container");
     data.splice(0, 1);
     data.forEach((dato) => {
-      let id=document.createElement("h6");
+      let id = document.createElement("h6");
       id.classList.add("fw-normal", "text-secondary");
-      id.className="id-product";
-      id.innerText= dato._id;
-      id.style.fontSize="13px";
-      id.style.marginBottom="20px";
+      id.className = "id-product";
+      id.innerText = dato._id;
+      id.style.fontSize = "13px";
+      id.style.marginBottom = "20px";
       let card = document.createElement("div");
-      card.classList="card";
-      card.style.backgroundColor="white";
-      card.classList.add("mt-3", "mb-3")
+      card.classList = "card";
+      card.style.backgroundColor = "white";
+      card.classList.add("mt-3", "mb-3");
       card.style.width = "300px";
       card.classList.add("p-2");
-      card.style.borderRadius="10px";
+      card.style.borderRadius = "10px";
       card.style.height = "670px";
-      card.classList.add(
-        "d-flex",
-        "flex-column",
-        "align-items-center"
-      );
-
+      card.classList.add("d-flex", "flex-column", "align-items-center");
       let nomeProdotto = document.createElement("h3");
       nomeProdotto.innerText = dato.name;
       nomeProdotto.style.fontSize = "20px";
       nomeProdotto.style.marginTop = "20px";
       nomeProdotto.style.marginBottom = "10px";
       nomeProdotto.classList.add("text-center");
+      nomeProdotto.classList.add("titolo-prodotto");
       let marca = document.createElement("h4");
       marca.innerText = dato.brand;
       marca.classList.add("text-center");
+      marca.classList.add("text-primary");
+      marca.classList.add("marca-prodotto");
       let descrizione = document.createElement("p");
       descrizione.style.overflow = "hidden";
       descrizione.style.textOverflow = "ellipsis";
@@ -143,10 +135,11 @@ const fetchData = async () => {
       descrizione.innerText = dato.description;
       let prezzo = document.createElement("h4");
       prezzo.innerText = dato.price + " €";
+      prezzo.classList.add("text-primary");
       let immagine = document.createElement("img");
       immagine.src = dato.imageUrl;
       immagine.style.width = "250px";
-      immagine.style.marginBottom="20px";
+      immagine.style.marginBottom = "20px";
       let pulsanti = document.createElement("div");
       pulsanti.classList.add("d-flex");
       pulsanti.classList.add("justify-content-end");
@@ -159,20 +152,19 @@ const fetchData = async () => {
       let elimina = document.createElement("div");
       elimina.style.width = "50px";
       elimina.style.height = "50px";
-      elimina.style.border="1px solid black";
+      elimina.style.border = "1px solid black";
       elimina.classList.add(
         "d-flex",
         "justify-content-center",
         "align-items-center",
         "btn",
         "btn-primary"
-       
       );
       let modifica = document.createElement("div");
       modifica.style.width = "50px";
-      modifica.style.border="1px solid black";
+      modifica.style.border = "1px solid black";
       modifica.style.height = "50px";
-      modifica.style.marginRight="10px";
+      modifica.style.marginRight = "10px";
       modifica.classList.add(
         "d-flex",
         "justify-content-center",
@@ -185,38 +177,45 @@ const fetchData = async () => {
       deleteIcon.classList.add("bi-trash3-fill");
       deleteIcon.classList.add("fs-4");
       deleteIcon.classList.add("cursor-pointer");
-      
+
       let modifyIcon = document.createElement("i");
       modifyIcon.classList.add("bi");
       modifyIcon.classList.add("bi-pencil");
       modifyIcon.classList.add("fs-4");
       modifyIcon.classList.add("cursor-pointer");
       modifica.addEventListener("click", function () {
-        let formModificaProdotto = document.getElementById('modifica-prodotti');
+        let formModificaProdotto = document.getElementById("modifica-prodotti");
         //Quando clicco sull'icona modifica mi porta al modulo per modificare il mio prodotto
-        formModificaProdotto.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        let modifiedNameInput=document.getElementById("modified-name");
-        modifiedNameInput.value=nomeProdotto.innerText;
-        let modifiedDescriptionInput=document.getElementById("modified-description");
-        modifiedDescriptionInput.value=descrizione.innerText;
-        let modifiedPriceInput=document.getElementById("modified-price");
-        modifiedPriceInput.value=parseFloat(prezzo.innerText);
-        let modifiedBrandInput=document.getElementById("modified-brand");
-        modifiedBrandInput.value=marca.innerText;
-        let modifiedIdInput=document.getElementById("modified-id");
-        modifiedIdInput.value=id.innerText;
-        let modifiedImgInput=document.getElementById("modified-imageUrl");
-        modifiedImgInput.value=immagine.src;
-        
-      })
-      elimina.addEventListener("click", function() {
-        let formEliminaProdotto=document.getElementById("elimina-prodotti");
-        formEliminaProdotto.scrollIntoView({ behavior: 'smooth', block: 'start' })
-        let idInput=document.getElementById("delete-product-id");
-        idInput.value=id.innerText;
+        formModificaProdotto.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+        let modifiedNameInput = document.getElementById("modified-name");
+        modifiedNameInput.value = nomeProdotto.innerText;
+        let modifiedDescriptionInput = document.getElementById(
+          "modified-description"
+        );
+        modifiedDescriptionInput.value = descrizione.innerText;
+        let modifiedPriceInput = document.getElementById("modified-price");
+        modifiedPriceInput.value = parseFloat(prezzo.innerText);
+        let modifiedBrandInput = document.getElementById("modified-brand");
+        modifiedBrandInput.value = marca.innerText;
+        let modifiedIdInput = document.getElementById("modified-id");
+        modifiedIdInput.value = id.innerText;
+        let modifiedImgInput = document.getElementById("modified-imageUrl");
+        modifiedImgInput.value = immagine.src;
+      });
+      elimina.addEventListener("click", function () {
+        let formEliminaProdotto = document.getElementById("elimina-prodotti");
+        formEliminaProdotto.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+        let idInput = document.getElementById("delete-product-id");
+        idInput.value = id.innerText;
         console.log(idInput);
         console.log(id.innerText);
-      })
+      });
       elimina.appendChild(deleteIcon);
       modifica.appendChild(modifyIcon);
       pulsanti.appendChild(modifica);
@@ -230,14 +229,8 @@ const fetchData = async () => {
       card.appendChild(prezzo);
 
       container.appendChild(card);
-
-     
-
     });
 
-
-
-   
     // Ora puoi manipolare i dati come desideri, ad esempio visualizzarli nella tua interfaccia utente
   } catch (error) {
     console.error("Si è verificato un errore durante la fetch:", error);
@@ -246,4 +239,39 @@ const fetchData = async () => {
 
 // Chiamare la funzione per recuperare i dati dal database
 fetchData();
+
+function search() {
+  let cards = document.getElementsByClassName("card");
+  let titoliProdotto = document.getElementsByClassName("titolo-prodotto");
+  let marcaProdotto = document.getElementsByClassName("marca-prodotto");
+  let cardsContainer = document.getElementById("cards-container");
+  let searchButton = document.getElementById("search-button");
+  let searchInput = document.getElementById("search-input");
+  let valoreInput = searchInput.value.toLowerCase();
+  console.log(valoreInput);
+  let lensIcon = document.getElementById("lens");
+  lensIcon.addEventListener("click", function () {
+    cardsContainer.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+
+  searchButton.addEventListener("click", function () {
+    cardsContainer.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+
+  for (let i = 0; i < cards.length; i++) {
+    let titolo = titoliProdotto[i].textContent.toLowerCase(); // Estrai il testo del titolo e convertilo in minuscolo
+    console.log(titolo);
+    let marca=marcaProdotto[i].textContent.toLowerCase();
+    console.log(marca)
+
+    if ((titolo.includes(valoreInput)) || (marca.includes(valoreInput))) {
+     cards[i].classList.remove("invisible")
+    } else {
+      cards[i].classList.add("invisible"); // Nascondi la card se il titolo non contiene la parola chiave
+    }
+  }
+}
+
+
+
 
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ row.classList.add("row");
 document.body.appendChild(row); // Aggiungo la riga al corpo del documento
 
 let titleSection = document.createElement("h3"); // Creo un nuovo elemento h2 per il titolo
-titleSection.innerHTML = `<h3>Tutti i prodotti<i class="fa-solid fa-star ms-3"></i></h3>`;
+titleSection.innerHTML = `<h3 style="margin-left:270px">Tutti i prodotti<i class="fa-solid fa-star ms-3"></i></h3>`;
 titleSection.classList.add("my-font");
 titleSection.classList.add("mt-3");
 
@@ -15,11 +15,125 @@ row.appendChild(titleSection);
 row.classList.add("text-center");
 row.classList.add("mt-4");
 
-//Creo un container per le cards
+// Create bigContainer
+let bigContainer = document.createElement("div");
+bigContainer.classList.add("container-fluid");
+bigContainer.classList.add("bigContainer");
+bigContainer.style.display = "flex"; // Setting display to flex
+
+// Append bigContainer to body
+document.body.appendChild(bigContainer);
+
+// Create aside
+let aside = document.createElement("div");
+aside.classList.add("flex-shrink-0");
+aside.classList.add("p-3");
+aside.style.width = "280px"; // Setting width
+aside.innerHTML = `
+<div class="flex-shrink-0 p-3" style="width: 280px;">
+    <ul class="list-unstyled ps-0 ">
+    <li class="fs-3 mb-4 fw-semibold  ms-4">Filtri  <i class="bi bi-sliders ms-2"></i></li>
+      <li class="mb-1">
+     <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed fs-4 fw-semibold w-100 " data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+          <span class="w-50">Brand</span> <i class="fa-solid fa-chevron-right ms-5 w-50"></i>
+        </button>
+        <div class="collapse" id="home-collapse" style="">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded fs-5 ms-4">
+            <input type="checkbox" id="barbie" class="me-2" value="barbie">
+            <label for="barbie">Barbie</label>
+
+            </a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded fs-5 ms-4">
+            <input type="checkbox" id="frozen" class="me-2" value="disney-frozen">
+            <label for="frozen">Disney Frozen</label>
+            </a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded fs-5 ms-4">
+            <input type="checkbox" id="princess" class="me-2" value="disney-princess">
+            <label for="princess">Disney Princess</label>
+            </a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="list-divider"></li>
+      <li class="mb-1">
+        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed fs-4 fw-semibold w-100" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+          <span class="w-50 ms-2">Categoria </span><i class="fa-solid fa-chevron-right ms-4 w-50"></i>
+        </button>
+        <div class="collapse" id="dashboard-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded ms-4 fs-5">
+            <input type="checkbox" id="bambole" class="me-2" value="bambole">
+            <label for="bambole" >Bambole</label>
+            </a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded ms-4 fs-5">
+            <input type="checkbox" id="bambole-collezione" class="me-2" value="bambole-collezione">
+            <label for="bambole collezione">Bambole da collezione</label>
+            </a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded ms-4 fs-5">
+            <input type="checkbox" id="casa-bambole" class="me-2" value="casa-bambole">
+            <label for="casa-bambole">Casa delle bambole</label>
+            </a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="list-divider"></li>
+      <li class="mb-1">
+        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed fs-4 fw-semibold w-100" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+          <span class="w-50 ms-1">Prezzo</span><i class="fa-solid fa-chevron-right ms-5 w-50"></i>
+        </button>
+        <div class="collapse" id="orders-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded fs-5 ms-4">
+            <input type="checkbox" id="fascia-prezzo-1" class="me-2" value="0-25">
+            <label for="fascia-prezzo-1">0-25 €</label>
+            </a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded ms-4 fs-5">
+            <input type="checkbox" id="fascia-prezzo-2" class="me-2" value="25-50">
+            <label for="fascia-prezzo-2">25-50 €</label>
+            </a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded ms-4 fs-5">
+            <input type="checkbox" id="fascia-prezzo-3" class="me-2" value="50-100">
+            <label for="fascia-prezzo-3">50-100 €</label>
+            </a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="list-divider"></li>
+      <li class="mb-1">
+      <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed fs-4 fw-semibold w-100" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+        <span class="w-25 ms-2 me-5">Età</span><i class="fa-solid fa-chevron-right ms-5 w-75"></i>
+      </button>
+      <div class="collapse" id="orders-collapse">
+        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+          <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded fs-5 ms-4">
+          <input type="checkbox" id="fascia-età-1" class="me-2">
+          <label for="fascia-età-1">2-4</label>
+          </a></li>
+          <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded ms-4 fs-5">
+          <input type="checkbox" id="fascia-età-2" class="me-2">
+            <label for="fascia-età-2">5+ </label>
+          </a></li>
+    
+    
+        </ul>
+      </div>
+    </li>
+    <li class="list-divider"></li>
+    </ul>
+  </div>`;
+
+// Append aside to bigContainer
+bigContainer.appendChild(aside);
+
+// Create container for cards
 let container = document.createElement("div");
 container.classList.add("container");
 container.classList.add("cards-container");
-document.body.appendChild(container);
+
+// Append container to bigContainer
+bigContainer.appendChild(container);
+
 container.classList.add("mt-4");
 container.classList.add("d-flex");
 container.classList.add("gap-5");
@@ -40,20 +154,26 @@ fetch("https://striveschool-api.herokuapp.com/api/product/", {
     let dati = data;
     dati.splice(0, 1);
     dati.forEach((dato) => {
-      let id=dato._id;
+      let id = dato._id;
       let card = document.createElement("div");
+      card.classList.add("product-card");
       card.classList.add("d-flex", "flex-column", "align-items-center");
       card.style.height = "500px";
       card.classList.add("p-2");
       card.style.width = "350px";
       card.style.borderRadius = "5px";
+      let marca = document.createElement("h5");
+      marca.innerText = dato.brand;
+      marca.classList.add("marca-prodotto", "text-primary");
       let img = document.createElement("img");
       img.src = dato.imageUrl;
       img.style.width = "230px";
       img.classList.add("mt-3", "cursor-pointer");
-      img.onclick=() => (window.location=`productPage.html?productId=${id}`);
+      img.onclick = () =>
+        (window.location = `productPage.html?productId=${id}`);
       let nome = document.createElement("h5");
       nome.innerText = dato.name;
+      nome.classList.add("titolo-prodotto");
       nome.style.overflow = "hidden";
       nome.style.textOverflow = "ellipsis";
       nome.style.whiteSpace = "nowrap";
@@ -69,6 +189,7 @@ fetch("https://striveschool-api.herokuapp.com/api/product/", {
 
       card.appendChild(img);
       card.appendChild(nome);
+      card.appendChild(marca);
       card.appendChild(prezzo);
       card.appendChild(bottone);
       container.appendChild(card);
@@ -77,3 +198,87 @@ fetch("https://striveschool-api.herokuapp.com/api/product/", {
   .catch((error) => {
     console.error("Si è verificato un errore:", error);
   });
+
+function search() {
+  let cards = document.getElementsByClassName("product-card");
+  let titoliProdotto = document.getElementsByClassName("titolo-prodotto");
+  let marcaProdotto = document.getElementsByClassName("marca-prodotto");
+  console.log(marcaProdotto);
+  let cardsContainer = document.querySelector(".cards-container");
+  let searchButton = document.getElementById("search-button");
+  let searchInput = document.getElementById("search-bar");
+  let valoreInput = searchInput.value.toLowerCase();
+  console.log(valoreInput);
+  let lensIcon = document.getElementById("lens");
+  lensIcon.addEventListener("click", function () {
+    cardsContainer.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+
+  searchButton.addEventListener("click", function () {
+    cardsContainer.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+
+  for (let i = 0; i < cards.length; i++) {
+    let titolo = titoliProdotto[i].textContent.toLowerCase(); // Estrai il testo del titolo e convertilo in minuscolo
+    let marca = marcaProdotto[i].textContent.toLowerCase();
+
+    if (titolo.includes(valoreInput) || marca.includes(valoreInput)) {
+      cards[i].classList.remove("invisible");
+    } else {
+      cards[i].classList.add("invisible"); // Nascondi la card se il titolo non contiene la parola chiave
+    }
+  }
+}
+
+function filtroPerBrand() {
+  let cards = document.getElementsByClassName("product-card");
+  let marcaProdotto = document.getElementsByClassName("marca-prodotto");
+  let barbieSelect = document.getElementById("barbie");
+  let frozenSelect = document.getElementById("frozen");
+  let princessSelect = document.getElementById("princess");
+  let barbieValue = barbieSelect.checked;
+  let frozenValue = frozenSelect.checked;
+  let princessValue = princessSelect.checked;
+
+
+  // Controlla se nessuna checkbox è selezionata
+  if (!barbieValue && !frozenValue && !princessValue) {
+    // Nessuna checkbox è selezionata, mostrare tutte le cards
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].classList.remove("invisible");
+    }
+    // Uscire dalla funzione in quanto non c'è bisogno di ulteriori filtri
+    return;
+}
+
+
+  for (let i = 0; i < cards.length; i++) {
+    let marca = marcaProdotto[i].textContent.toLowerCase();
+    let showCard = false;
+
+    if (
+      (barbieValue && marca.includes("barbie")) ||
+      (frozenValue && marca.includes("frozen")) ||
+      (princessValue && marca.includes("princess"))
+    ) {
+      showCard = true;
+    }
+
+    if (showCard) {
+      cards[i].classList.remove("invisible");
+    } else {
+      cards[i].classList.add("invisible");
+    }
+  }
+}
+
+// Ottenere le referenze alle checkbox
+let barbieSelect = document.getElementById("barbie");
+let frozenSelect = document.getElementById("frozen");
+let princessSelect = document.getElementById("princess");
+
+// Aggiungere un gestore di eventi per l'evento "click" a ciascuna checkbox
+barbieSelect.addEventListener("click", filtroPerBrand);
+frozenSelect.addEventListener("click", filtroPerBrand);
+princessSelect.addEventListener("click", filtroPerBrand);
+
